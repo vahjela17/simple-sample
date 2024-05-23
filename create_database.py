c01e99056f8387e753e4b524125be35122ca0ca9
vahjela17 <109843773+vahjela17@users.noreply.github.com>
2024-05-23 04:12:19 -0400

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -14,7 +14,9 @@ products_table = Table(
 
 metadata.create_all(engine)
 
-excel_path = r"C:\Users\mikev\Downloads\SimpleSample\simple-sample\PricingReport_20240516_203315894_4588.xlsx"
+excel_path = file_path = os.path.join(app.root_path, "PricingReport_20240516_203315894_4588.xlsx"
+if not os.path.exists(file_path):
+    return jsonify({"error": "Template file not found"}), 404
 df = pd.read_excel(excel_path, sheet_name='05-16-2024 Pricing')
 
 df = df.rename(columns={'Product ID': 'ProductID', 'Product Name': 'ProductName', 'Unit Price': 'UnitPrice'})
